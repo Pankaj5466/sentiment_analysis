2e34d238c89d6ecc1dff3ebe1398f2e233091d23
EC2 Default User <ec2-user@ip-172-16-15-59.ap-south-1.compute.internal>
2020-05-13 01:34:27 +0000

    First Commit

--- serve/predict.py
+++ serve/predict.py
@@ -70,8 +70,8 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = None
-    data_len = None
+    data_word_list = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict,data_word_list)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
